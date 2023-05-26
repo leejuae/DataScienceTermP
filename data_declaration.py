fc4c91b0fadf012e97db8ad6dfdb779512b56bd3
leejuae <joojuae02@gachon.ac.kr>
2023-05-26 19:24:38 +0900

    2023 bus station data feature 다른거 수정함 + B, K로 저장된 호선 수정

---
+++ data_declaration.py
@@ -0,0 +1,27 @@
+import pandas as pd
+# 외부코드,전철역명,호선,정류장명,정류장ID,(X)좌표,(Y)좌표
+df_nearestBusStation = pd.read_csv("D:\git\DataScienceTermP\\nearest_busStation.csv", encoding='euc-kr')
+
+df_BusStation2201 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202201.csv", encoding='euc-kr')
+df_BusStation2202 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202202.csv", encoding='euc-kr')
+df_BusStation2203 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202203.csv", encoding='euc-kr')
+df_BusStation2204 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202204.csv", encoding='euc-kr')
+df_BusStation2205 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202205.csv", encoding='euc-kr')
+df_BusStation2206 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202206.csv", encoding='euc-kr')
+df_BusStation2207 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202207.csv", encoding='euc-kr')
+df_BusStation2208 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202208.csv", encoding='euc-kr')
+df_BusStation2209 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202209.csv", encoding='euc-kr')
+df_BusStation2210 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202210.csv", encoding='euc-kr')
+df_BusStation2211 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202211.csv", encoding='euc-kr')
+df_BusStation2212 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202212.csv", encoding='euc-kr')
+df_BusStation2301 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202301.csv", encoding='euc-kr')
+df_BusStation2302 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202302.csv", encoding='euc-kr')
+df_BusStation2303 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202303.csv", encoding='euc-kr')
+df_BusStation2304 = pd.read_csv("D:\git\DataScienceTermP\\busStation\\busStation202304.csv", encoding='euc-kr')
+
+
+df_subway_202301 = pd.read_csv("D:\git\DataScienceTermP\subway\subway202301.csv", encoding='utf-8')
+df_subway_202302 = pd.read_csv("D:\git\DataScienceTermP\subway\subway202302.csv", encoding='utf-8')
+df_subway_202303 = pd.read_csv("D:\git\DataScienceTermP\subway\subway202303.csv", encoding='utf-8')
+df_subway_202304 = pd.read_csv("D:\git\DataScienceTermP\subway\subway202304.csv", encoding='utf-8')
+df_subway_2022 = pd.read_csv("D:\git\DataScienceTermP\subway\subway2022.csv", encoding='utf-8')
