ee9346a9200cc1731aa1c69390bc4b2abf713240
leejuae <joojuae02@gachon.ac.kr>
2023-06-01 22:04:10 +0900

    파일 수정 및 normalization필요사항 정

--- datasetTest.py
+++
@@ -1,8 +0,0 @@
-#데이터셋 test file
-import data_declaration
-import pandas as pd
-
-df = data_declaration.df_nearestBusStation
-non_int = df[~df['호선'].astype(str).str.isdigit()]
-for _, row in non_int.iterrows():
-    print(f"전철역명: {row['전철역명']}, 호선: {row['호선']}")
